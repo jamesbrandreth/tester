6c0c347473373292112ff62ecc0d2621c66f4462
James <james.brandreth@gmail.com>
2021-02-23 00:01:04 +0000

    Initial diff parsing

--- tester/git/diff.py
+++ tester/git/diff.py
@@ -1,2 +1,22 @@
+import regex
+from typing import Optional, Tuple#
+from enum import Enum
+
+class DiffType(Enum):
+	create: 'create'
+	change: 'change'
+	delete: 'delete'
+
+
+class Diff:
+	def __init__(self, diff_type: DiffType, filepath: str, change_range: Optional[Tuple[int, int]]):
+		pass
 
 def diff(paths):
+	difflist = []
+	changelist = []
+
+	for diff in difflist:
+		pass
+
+	return changelist
