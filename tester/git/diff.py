5a8970eb27c0481a749f92b70d3241b7c1b6f8c7
James <james.brandreth@gmail.com>
2021-02-24 18:30:38 +0000

    Created Diff class

--- tester/git/diff.py
+++ tester/git/diff.py
@@ -1,22 +1,39 @@
-import regex
-from typing import Optional, Tuple#
+import subprocess
+import re
+from typing import Optional, List, Tuple
 from enum import Enum
 
+DIFF_REGEX = re.compile(r"^diff --git a.*b(.*?)\n(\n|.)*?@@ [+-]\d+,\d+ [+-](\d+),(\d+)", flags=re.MULTILINE)
+
+
 class DiffType(Enum):
-	create: 'create'
-	change: 'change'
-	delete: 'delete'
+	Create = 'Create'
+	Change = 'Change'
+	Delete = 'Delete'
 
 
 class Diff:
 	def __init__(self, diff_type: DiffType, filepath: str, change_range: Optional[Tuple[int, int]]):
-		pass
+		self.diff_type = diff_type
+		self.filepath = filepath
+		self.change_range = change_range
 
-def diff(paths):
-	difflist = []
+	def __repr__(self):
+		return f"Diff object >> type: {self.diff_type}, file: {self.filepath}, lines: {self.change_range}>"
+
+
+def diff():
 	changelist = []
 
-	for diff in difflist:
-		pass
+	diff_output = subprocess.check_output(["git", "diff-index", "-p", "HEAD"]).decode('utf8')
+	for d in re.finditer(DIFF_REGEX, diff_output):
+		filepath = d.group(1)
+		start = int(d.group(3))
+		end = int(d.group(4))
+		changelist.append(Diff(DiffType.Change, filepath, (start, end)))
 
 	return changelist
+
+
+if __name__ == '__main__':
+	print(diff())
